98ce241d252f8fc7bd5ee1c31b64baa1065cc5ee
papatriz <papatriz@gmail.com>
2022-12-23 15:55:23 +0200

    getting and showing tracking for last 7 days

--- src/main/java/com/papatriz/dailyfe/controller/MainController.java
+++ src/main/java/com/papatriz/dailyfe/controller/MainController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Getter
@@ -26,12 +28,14 @@ public class MainController {
     @Value("${restapi.root}")
     private String restApiRoot;
     private final Map<String, String> restEndPoints =
-            Map.of("USER_DATA","/user","ACTIVITIES","/activity");
+            Map.of("USER_DATA","/user","ACTIVITIES","/activity", "CHECK_ACTIVITY", "/activity/{id}/{date}");
+    private LocalDate startDate;
 
     @PostConstruct
     private void init() {
         var url = restApiRoot + restEndPoints.get("USER_DATA");
         userData = restTemplate.getForObject(url, UserDto.class);
+        startDate = LocalDate.now();
         logger.info("Get user data from remote server: "+userData);
         logger.info("Rest API root: "+restApiRoot);
     }
@@ -40,5 +44,28 @@ public class MainController {
         var url = restApiRoot + restEndPoints.get("ACTIVITIES");
 
         return restTemplate.getForObject(url, ActivityDto[].class);
+    }
+
+    public String getDateRepresent(int dateShift) {
+       var targetDate = startDate.plusDays(dateShift);
+       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM");
+       return targetDate.format(formatter);
+    }
+
+    public boolean isActivityComplete(long activityId, int dateShift) {
+        var urlTemplate = restApiRoot + restEndPoints.get("CHECK_ACTIVITY");
+
+        var targetDate = startDate.plusDays(dateShift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        var url = urlTemplate.replace("{id}", String.valueOf(activityId))
+                                     .replace("{date}", targetDate.format(formatter));
+
+        logger.info("Check activity completion by URL: "+url);
+        return Boolean.TRUE.equals(restTemplate.getForObject(url, Boolean.class));
+    }
+
+    public boolean getRandomBool() { // for test purposes only
+        var time = System.nanoTime();
+        return time % 2 == 0;
     }
 }
